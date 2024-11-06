ff881028a26ab746e61a98d3300425bd5df2b900
OP <106511969+Op-Pautu@users.noreply.github.com>
2024-11-06 16:31:32 +0530

    fetch data from db and render in topics-list in home page

--- src/components/topics-list.tsx
+++ src/components/topics-list.tsx
@@ -1,28 +1,50 @@
-import {
-  Edit,
-  Edit2Icon,
-  PencilIcon,
-  Trash,
-  Trash2,
-  TrashIcon,
-} from "lucide-react";
+import { Edit, Trash2 } from "lucide-react";
 import Link from "next/link";
 
-export const TopicsList = () => {
+interface Topic {
+  _id: string;
+  title: string;
+  description: string;
+}
+
+const getTopics = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_LOCAL_URL}/api/topics`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics", error);
+  }
+};
+
+export const TopicsList = async () => {
+  const { topics } = await getTopics();
+
   return (
     <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-        <div>
-          <h2 className="font-bold text-2xl">Topic Title</h2>
-          <p>Topic Description</p>
+      {topics.map((topic: Topic) => (
+        <div
+          key={topic._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{topic.title}</h2>
+            <p>{topic.description}</p>
+          </div>
+          <div className="flex gap-2">
+            <Trash2 size={24} className="text-red-400" />
+            <Link href={`/edit-topic/${topic._id}`}>
+              <Edit size={24} />
+            </Link>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <Trash2 size={24} className="text-red-400" />
-          <Link href={"/edit-topic"}>
-            <Edit size={24} />
-          </Link>
-        </div>
-      </div>
+      ))}
     </>
   );
 };
